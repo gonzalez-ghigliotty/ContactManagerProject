8ba002f80cd0a0830d5c71daf7080b0bee4b20ca
Austin Ghigliotty <austin.ghigliotty@gmail.com>
2019-08-08 14:40:43 -0500

    search complete

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -22,11 +22,9 @@ public class ContactManager {
         if (response.contains("1")) {
             Contacts.main();
             System.out.println();
-            System.out.println("Press '0' to return to menu.");
+            System.out.println("Press any key to return to menu.");
             String back = scan.next();
-            if (back.contains("0")); {
                 main(null);
-            }
         } else if (response.contains("2")) {
             String newContact;
             System.out.println("Enter first name of contact: ");
@@ -48,29 +46,48 @@ public class ContactManager {
 
 
         } else if (response.contains("3")) {
-            String newSearch;
-            System.out.println("Search for your contact.");
-            newSearch = scan.next();
-            Path contactPath = Paths.get("src/","Contacts.txt");
-            List<String> contactList = null;
-            try {
-                contactList = Files.readAllLines(contactPath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            List<String> filteredList = null;
+            do {
+                String newSearch;
+                System.out.println("Search for your contact.");
+                newSearch = scan.next();
+                Path contactPath = Paths.get("src/", "Contacts.txt");
+                List<String> contactList = null;
+                try {
+                    contactList = Files.readAllLines(contactPath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                List<String> filteredList = null;
 
+                System.out.printf("%-15s|%-15s|", "     Name", " Phone Number");
+                System.out.println();
+                System.out.println("--------------------------------");
+                for (int i = 0; i < contactList.size(); i++) {
+                    String checkName = contactList.get(i);
+//                System.out.println(checkName);
+                    String[] filterSplitName = checkName.split(" ");
+                    if (filterSplitName[0].contains(newSearch)) {
+                        System.out.printf("%-15s|%-15s", filterSplitName[0], filterSplitName[1]);
+                        System.out.println();
+                    }
+                }
+                System.out.println();
+                System.out.println("Would you like to search again? (y/n)");
+                boolean asking = true;
+                do {
+                String back = scan.next();
+                    if (back.contains("n")) {
+                        main(null);
+                        asking = false;
+                    } else if (back.contains("y")) {
+                        break;
+                    } else {
+                        System.out.println("Please give a valid answer.");
+                    }
+                } while (asking);
+            } while (true);
+        } else if (response.contains("4")) {
 
-            for (int i = 0; i < contactList.size(); i ++) {
-                String checkName = contactList.get(i);
-                System.out.println(checkName);
-//                checkName.getName("");
-//                String[] splitName = newName.split(" ");
-//            }
-
-
-//                System.out.println(token);
-            }
         }
     }
 }
