9dfb1059d8497485683434b8a2a76ca0ba9f185b
Your Name <you@example.com>
2019-08-09 12:03:06 -0500

    Everything works

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -12,22 +12,22 @@ public class ContactManager {
     public static void main(String[] args) {
 
         boolean addedContact = false;
-        System.out.println("1. View Contacts.\n" +
-                "2. Add a new contact.\n" +
-                "3. Search a contact by name.\n" +
-                "4. Delete an existing contact.\n" +
-                "5. Exit.\n" +
+        System.out.println("1. View Contacts.\uD83D\uDC40\n" +
+                "2. Add a new contact.➕\n" +
+                "3. Search a contact by name.\uD83D\uDD0D\n" +
+                "4. Delete an existing contact.\uD83D\uDEB7\n" +
+                "5. Exit.✌️\n" +
                 "Enter an option (1, 2, 3, 4 or 5):");
         Scanner scan = new Scanner(System.in);
         String response = scan.nextLine();
-        System.out.println(response);
-
+//          TODO: Create a if to output the contacts.txt file. and to return to the main menu.
         if (response.equals("1")) {
             Contacts.main();
             System.out.println();
             System.out.println("Press any key to return to menu.");
             String back = scan.next();
                 main(null);
+//           TODO: ADD a else if to allow the user to add a contact.
         } else if (response.equals("2")) {
             String newContact;
             System.out.println("Enter first name of contact: ");
@@ -35,6 +35,7 @@ public class ContactManager {
             System.out.println("Enter phone number of contact: ");
             String newNumber = scan.next();
             newContact = newName + " " + newNumber;
+//            TODO: Create the try/catch to avoid getting errors and to prompt the user of existing contact.
 
             Path contactPath = Paths.get("src/", "Contacts.txt");
             List<String> contactList = null;
@@ -90,7 +91,7 @@ public class ContactManager {
                 main(null);
             }
 
-
+//            TODO: Allow the user to search for a contact and if more than one contact share a same character, an array list will drop down.
         } else if (response.equals("3")) {
             do {
                 String newSearch;
@@ -131,6 +132,7 @@ public class ContactManager {
                     }
                 } while (asking);
             } while (true);
+//            TODO: Prompt the user about deleting a contact and making sure the input is a actual contact.
         } else if (response.equals("4")) {
             System.out.println("Who do you wish to shun?");
             String delete = scan.next();
@@ -151,16 +153,12 @@ public class ContactManager {
                     System.out.println("Are you sure you wish to shun contact?");
                     String confirm = scan.next();
                     try {
-//                        Files.write(
                                 contactList.remove(contactList.get(i));
                         PrintWriter writer = new PrintWriter("src/contacts.txt");
                         for (int ii = 0; ii < contactList.size(); ii++)
                             writer.println(contactList.get(ii));
                         writer.close();
-//                                Paths.get("src/", "Contacts.txt"),
-//                                Arrays.asList(deleteSplitName), // list with one item
-//                                StandardOpenOption.
-//                        );
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -177,7 +175,7 @@ public class ContactManager {
                 String back = scan.next();
                 main(null);
             }
-
+//            TODO: Create a functioning exit option.
         } else if (response.equals("5")) {
             System.out.println("LATERSSSSSSSSSSS \uD83E\uDD2A");
             System.exit(0);
